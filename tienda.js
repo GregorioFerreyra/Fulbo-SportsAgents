592881572112b4f1dbe991cdc9c39e82498f20b5
GregorioFerreyra <gregorioferreyralammertyn@gmail.com>
2024-01-13 21:46:28 -0300

    esta

--- tienda.js
+++ tienda.js
@@ -97,7 +97,7 @@
 /***********************************Fetch de Ruta Relativa para las cards ********************/
 
 
-        fetch('./data/ecommerce.json')
+        fetch('/pages/ecommerce.json')
             .then((response) => response.json())
             .then((data) => {
                 data.forEach((product, index) => {
